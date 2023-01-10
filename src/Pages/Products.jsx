59dabf1d440187fe8b521694625951b47335422d
Rohan1279 <muniruddinrohan@gmail.com>
2023-01-10 13:41:10 +0600

    add filter button in products page

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import BannerCard from "../components/BannerCard/BannerCard";
 import { Context } from "../contexts/ContextProvider";
@@ -9,6 +9,16 @@ import Button from "../components/Button/Button";
 //! add pagination feature
 const Products = () => {
   const { products, categories } = useContext(Context);
+  const [categoryProducts, setCategoryProducts] = useState([]);
+  const handleFilterProducts = (id) => {
+    const filteredProducts = products.filter((product) =>
+      product.category_id.includes(id)
+    );
+    setCategoryProducts(filteredProducts);
+  };
+  const handleBrowseProduct = (id) => {
+    console.log(id);
+  };
   return (
     <div>
       <h1 className="text-center text-5xl my-10">All Products</h1>
@@ -18,6 +28,7 @@ const Products = () => {
           <Button
             key={category.id}
             data={category}
+            handler={handleFilterProducts}
             classes={"w-36 rounded-md border-slate-300  mx-5"}
           >
             {category.name}
@@ -25,8 +36,10 @@ const Products = () => {
         ))}
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 transition-all">
-        {products?.map((product) => (
+        {categoryProducts?.map((product) => (
+          // <h1>{product.name}</h1>
           <BannerCard
+            handler={handleBrowseProduct}
             key={product._id}
             data={product}
             classes={"text-lg"}
