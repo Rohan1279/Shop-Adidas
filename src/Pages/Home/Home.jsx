0df277b3f3ec2146f84c425e980afc261ae4ed1b
Rohan1279 <muniruddinrohan@gmail.com>
2023-01-16 00:39:14 +0600

    add details in product details page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -7,6 +7,7 @@ const Home = () => {
   return (
     <div className="px-5">
       <Banner />
+     
     </div>
   );
 };
--- src/Pages/Products/ProductDetail.jsx
+++ src/Pages/Products/ProductDetail.jsx
@@ -5,11 +5,20 @@ import BannerCard from "../../components/BannerCard/BannerCard";
 const ProductDetail = () => {
   const { state } = useLocation();
   console.log(state);
-
   return (
-    <div>
+    <div className="px-5">
       <h1 className="text-center text-4xl my-5">Product Detail page</h1>
-      <BannerCard data={state} classes={"text-lg"}></BannerCard>
+      {/* <BannerCard data={state} classes={"text-lg"}></BannerCard> */}
+      <div className="flex gap-x-20 h-[44rem]">
+        <div className="w-1/2 h-fit">
+          <img src={state.img} alt="" className="w-full object-cover" />
+        </div>
+        <div className="w-1/2 shadow-nm px-7 py-5 rounded-md leading-loose">
+          <p>{state?.category}</p>
+          <h2 className="text-4xl font-extrabold mb-5">{state?.name}</h2>
+          <p className="text-xl">{state?.description}</p>
+        </div>
+      </div>
     </div>
   );
 };
