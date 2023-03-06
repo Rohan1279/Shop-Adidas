ab072e4c1509187c7c875ad22a1ebc2370cf2afd
Rohan1279 <muniruddinrohan@gmail.com>
2023-03-06 23:33:03 +0600

    add margin top in cart page

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Register = () => {
-  return <div className="h-screen">Register Page</div>;
+  return (
+    <div className="h-screen">
+      <h1>Register Page</h1>
+      
+    </div>
+  );
 };
 
 export default Register;
