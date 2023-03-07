b968eb2c29853ece72577a808e2ca36161bb6ba4
Rohan1279 <muniruddinrohan@gmail.com>
2023-03-07 12:14:34 +0600

    add auth functions in context provider

--- src/contexts/ContextProvider.jsx
+++ src/contexts/ContextProvider.jsx
@@ -1,13 +1,63 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { createContext, useEffect, useState } from "react";
+import app from "../firebase/firebase.init";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
 import { dataLoader } from "../utils/dataLoader";
 import { getStoredCart } from "../utils/fakeDB";
 
 export const Context = createContext();
+const auth = getAuth(app);
 const ContextProvider = ({ children }) => {
   const { products, categories, isLoading } = dataLoader();
-  
+  const [user, setUser] = useState({});
+  const [loading, setLoading] = useState(true);
+  const createUser = (email, passoword) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, passoword);
+  };
+  const updateUserProfile = (userInfo) => {
+    console.log(userInfo);
+    setLoading(true);
+    return updateProfile(auth.currentUser, userInfo);
+  };
+  const login = (email, passoword) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, passoword);
+  };
+  const authenticateWithProvider = (provider) => {
+    setLoading(true);
+    return signInWithPopup(auth, provider);
+  };
+  const logOut = () => {
+    localStorage.removeItem("accessToken");
+    return signOut(auth);
+  };
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+    return () => unsubscribe();
+  }, []);
+  const authInfo = {
+    user,
+    loading,
+    createUser,
+    updateUserProfile,
+    login,
+    authenticateWithProvider,
+    logOut,
+  };
+
   // const storedProducts = getStoredCart(); //! products with id
   // const initialCart = [];
   // for (const _id in storedProducts) {
@@ -27,6 +77,7 @@ const ContextProvider = ({ children }) => {
         value={{
           products,
           categories,
+          authInfo,
         }}
       >
         {children}
