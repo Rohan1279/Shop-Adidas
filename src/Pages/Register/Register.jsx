19cfa4a0984a0b4d46d2b1dad30d1c814e750af4
Rohan1279 <muniruddinrohan@gmail.com>
2023-03-08 01:25:23 +0600

    fix button click auto submit inside form

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -20,11 +20,12 @@ const Register = () => {
 
   const buyerRef = useRef();
   const sellerRef = useRef();
-  // console.log(userRole);
+  console.log(userRole);
   const handleAuthenticate = (provider) => {
     authenticateWithProvider(provider)
       .then((result) => {
         console.log(result.user);
+        console.log(userRole);
         // saveUser(result?.user.displayName, result?.user.email, userRole);
       })
       .catch((err) => {
@@ -64,32 +65,34 @@ const Register = () => {
         onClick={() => logOut()}
         className="text-2xl bg-secondary-color shadow-nm active:shadow-nm-inset"
       ></FaSignOutAlt>
-      <form onSubmit={handleRegister} className="card-body w-1/3 mx-auto">
-        <div className="form-control">
-          <div className="form-control ">
-            <label className="input-group  rounded-none">
-              <span className="label-text ">Your email</span>
-              <div className="flex  items-center border border-gray-300 rounded-md">
-                {/* <FaVoicemail className=""></FaVoicemail> */}
-                <img
-                  src="https://cdn2.iconfinder.com/data/icons/3d-basic/512/email_mail_message_envelope_communication.png"
-                  alt=""
-                  className="w-10 p-1"
-                />
-                <input
-                  type="text"
-                  placeholder="example@gmail.com"
-                  name="email"
-                  className="input input-bordered w-full bg-secondary-color shadow-nm-inset px-3 py-2"
-                  required
-                />
-              </div>
-            </label>
-          </div>
+      <form
+        onSubmit={handleRegister}
+        className="w-[28rem] mx-auto border border-gray-300 p-8 rounded-md shadow-nm"
+      >
+        <h1 className="text-center text-3xl font-extrabold">Create account</h1>
+        <div className="my-6">
+          <label className="input-group  rounded-none">
+            <span className="label-text ">Your email</span>
+            <div className="flex  items-center border border-gray-300 rounded-md">
+              {/* <FaVoicemail className=""></FaVoicemail> */}
+              <img
+                src="https://cdn2.iconfinder.com/data/icons/3d-basic/512/email_mail_message_envelope_communication.png"
+                alt=""
+                className="w-10 p-1"
+              />
+              <input
+                type="text"
+                placeholder="example@gmail.com"
+                name="email"
+                className="input input-bordered w-full bg-secondary-color shadow-nm-inset px-3 py-2"
+                required
+              />
+            </div>
+          </label>
         </div>
-        <div className="">
-          <div className="form-control">
-            <label className="input-group  ">
+        <div>
+          <div className="mb-6">
+            <label className=" ">
               <span>Your name</span>
               <div className="flex  items-center border border-gray-300 rounded-md">
                 {/* <FaVoicemail className=""></FaVoicemail> */}
@@ -107,17 +110,19 @@ const Register = () => {
                 />
               </div>
             </label>
-            <label className="input-group ">
+          </div>
+          <div className="mb-6">
+            <label className=" ">
               <span className="label-text">Password</span>
               <div className="flex  items-center border border-gray-300 rounded-md">
                 {/* <FaVoicemail className=""></FaVoicemail> */}
                 <img
-                  src="https://cdn3.iconfinder.com/data/icons/3d-basic/256/password_padlock_lock_privacy_lock.png"
+                  src="https://cdn4.iconfinder.com/data/icons/mix-3d-elements/256/7_lock_secure_security_padlock_protection_locker_password.png"
                   alt=""
                   className="w-10 p-1"
                 />
                 <input
-                  type="text"
+                  type="password"
                   name="password"
                   placeholder="password"
                   className="input input-bordered w-full bg-secondary-color shadow-nm-inset px-3 py-2"
@@ -128,30 +133,44 @@ const Register = () => {
           </div>
         </div>
         {/* user role */}
-        <div className="space-y-3 md:flex w-full justify-evenly items-center my-3">
-          <div className="form-control">
-            <label className="label cursor-pointer  flex items-center border border-gray-300 px-3 py-2 rounded-md ">
+        <h2 className="text-center my-3">Choose your role</h2>
+        <div className="w-full mx-auto flex items-center space-x-2 mb-6">
+          <div className="w-1/2">
+            {/* <label className="label cursor-pointer  flex items-center border border-gray-300 p-1 rounded-md focus:bg-blue-400"> */}
+
+            <button
+              // onChange={handleUserRole}
+              onClick={(e) => {
+                e.preventDefault();
+                setUserRole(buyerRef.current.innerText);
+              }}
+              type="radio"
+              name="radio-10"
+              className="cursor-pointer  flex items-center justify-center border border-gray-300 p-1 rounded-md focus:bg-blue-400 transition-all duration-300 w-full"
+              // checked
+              // defaultChecked
+            >
               <img
-                src="https://cdn1.iconfinder.com/data/icons/3d-isometric-color/256/bag-iso-color.png"
+                src="https://cdn3.iconfinder.com/data/icons/webina-seo-development-and-marketing/128/seo_web_3-68-256.png"
                 alt=""
                 className="w-10 h-10"
               />
-              <span className="label-text mx-3 text-xl" ref={buyerRef}>
+              <span className="label-text mx-3 text-xl tccc" ref={buyerRef}>
                 Buyer
               </span>
-              <input
-                // onChange={handleUserRole}
-                // onChange={() => setUserRole(buyerRef.current.innerText)}
-                type="radio"
-                name="radio-10"
-                className="checked:bg-yellow-500"
-                // checked
-                defaultChecked
-              ></input>
-            </label>
+            </button>
+            {/* </label> */}
           </div>
-          <div className="form-control">
-            <label className="label cursor-pointer  flex items-center border border-gray-300 px-3 py-2 rounded-md ">
+          <div className="w-1/2">
+            {/* <label className="label cursor-pointer  flex items-center border border-gray-300 p-1 rounded-md focus:bg-blue-400"> */}
+            <button
+              // onChange={handleUserRole}
+              onClick={(e) => {
+                e.preventDefault();
+                setUserRole(sellerRef.current.innerText);
+              }}
+              className="cursor-pointer  flex items-center justify-center border border-gray-300 p-1 rounded-md focus:bg-blue-400 transition-all duration-300 w-full"
+            >
               <img
                 src="https://cdn0.iconfinder.com/data/icons/3d-online-shop/256/icbsv2_7.png"
                 alt=""
@@ -160,25 +179,25 @@ const Register = () => {
               <span className="label-text mx-3 text-xl" ref={sellerRef}>
                 Seller
               </span>
-              <input
-                // onChange={handleUserRole}
-                onChange={() => setUserRole(sellerRef.current.innerText)}
-                type="radio"
-                name="radio-10"
-                className="radio checked:bg-fuchsia-500"
-              />
-            </label>
+            </button>
+            {/* </label> */}
           </div>
         </div>
-        <button
-          // onClick={handleRegister}
-          className="mx-auto bg-secondary-color shadow-nm p-2 text-3xl active:shadow-nm-inset"
-        >
-          Register
-        </button>
-        <div className="divider ">OR</div>
-        <hr className=" border-gray-400" />
-        <div className="flex  mx-auto gap-x-10">
+        <div>
+          <input
+            type="submit"
+            value="Register"
+            className="mx-auto bg-secondary-color border border-gray-300 p-2 text-xl active:shadow-nm-inset w-full rounded-md"
+          />
+          {/* <button
+            onClick={handleRegister}
+            className="mx-auto bg-secondary-color border border-gray-300 p-2 text-xl active:shadow-nm-inset w-full rounded-md"
+          >
+            Register
+          </button> */}
+        </div>
+        <hr className=" border-gray-300 my-3" />
+        <div className="flex justify-center space-x-3  mb-3">
           {/* <FaGoogle
             onClick={() => handleAuthenticate(googleProvider)}
             className="text-6xl bg-secondary-color shadow-nm p-2 rounded-full mx-auto active:shadow-nm-inset"
@@ -187,18 +206,23 @@ const Register = () => {
             onClick={() => handleAuthenticate(googleProvider)}
             src="https://cdn3.iconfinder.com/data/icons/3d-applications/256/app_icons_social_media_search___google_logo_engine_software.png"
             alt=""
-            className="w-12 h-12 bg-secondary-color shadow-nm p-2 rounded-full mx-auto active:shadow-nm-inset"
+            className="w-12 h-12 bg-secondary-color shadow-nm p-2 rounded-full active:shadow-nm-inset"
+          />
+          <img
+            src="https://cdn3.iconfinder.com/data/icons/3d-applications/256/app_icons_social_media___facebook_logo_social_network_media_online.png"
+            alt=""
+            className="w-12 h-12 bg-secondary-color shadow-nm p-2 rounded-full active:shadow-nm-inset"
           />
         </div>
-        <div className="form-control mt-6 ">
-          {/* <button className="btn btn-primary">
+        {/* <div className=" mt-6 ">
+          <button className="btn btn-primary">
             {isLoading ? (
               <SmallLoader />
             ) : (
               <input type="submit" value="Register" className="text-base" />
             )}
-          </button> */}
-        </div>
+          </button>
+        </div> */}
         <p className=" text-center">
           Already a user?{" "}
           <Link to={"/login"} className="text-orange-600 font-bold">
