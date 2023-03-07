0d0233215602d842ac5b2a8bf88329f4f9e48379
Rohan1279 <muniruddinrohan@gmail.com>
2023-03-07 12:28:11 +0600

    add google auth provider

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,10 +1,28 @@
-import React from "react";
-
+import { GithubAuthProvider, GoogleAuthProvider } from "firebase/auth";
+import React, { useContext } from "react";
+import { FaAppStore, FaGithub, FaGoogle } from "react-icons/fa";
+import { Context } from "../../contexts/ContextProvider";
+const googleProvider = new GoogleAuthProvider();
+// const githubProvider = new GithubAuthProvider();
 const Register = () => {
+  const { authInfo } = useContext(Context);
+  const { createUser, authenticateWithProvider, updateUserProfile } = authInfo;
+  const handleAuthenticate = (provider) => {
+    authenticateWithProvider(provider)
+      .then((result) => {
+        console.log(result.user);
+        saveUser(result?.user.displayName, result?.user.email, userRole);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
     <div className="h-screen">
-      <h1>Register Page</h1>
-      
+      <FaGoogle
+        onClick={() => handleAuthenticate(googleProvider)}
+        className="text-6xl bg-secondary-color shadow-nm p-2 rounded-full mx-auto"
+      ></FaGoogle>
     </div>
   );
 };
