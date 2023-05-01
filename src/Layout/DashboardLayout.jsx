308611b01f8fff770a208630a9e360d582b73b42
Rohan1279 <muniruddinrohan@gmail.com>
2023-05-01 13:08:35 +0600

    fix active button in drawer with tab component

--- src/Layout/DashboardLayout.jsx
+++ src/Layout/DashboardLayout.jsx
@@ -13,7 +13,7 @@ import {
 } from "react-icons/fa";
 import { HiXMark } from "react-icons/hi2";
 import Loader from "../components/Loader/Loader";
-import { Popover, Transition } from "@headlessui/react";
+import { Popover, Tab, Transition } from "@headlessui/react";
 
 const DashboardLayout = () => {
   const { authInfo } = useContext(Context);
@@ -51,39 +51,139 @@ const DashboardLayout = () => {
                   } transition-all ease-linear duration-300 
             `}
                 >
-                  <NavLink
+                  <Tab.Group>
+                    <Tab.List>
+                      <Tab as={Fragment}>
+                        {({ selected }) => (
+                          // <button
+                          //   className={
+                          //     selected
+                          //       ? "bg-primary-color text-gray-800 shadow-nm-inset hover: transition-all hover:text-gray-800 px-3 py-2 rounded-md text-sm font-medium border border-zinc-300 block "
+                          //       : "bg-primary-color text-gray-500 transition-all hover:text-gray-800 px-3 py-2 rounded-md text-sm font-medium border border-zinc-300 block "
+                          //   }
+                          // >
+                          //   <Link
+                          //     to={"/dashboard"}
+                          //     className={({ isActive }) => (isActive ? `` : "")}
+                          //   >
+                          //     <div className="flex justify-center items-center gap-x-2 ">
+                          //       <FaHome></FaHome>
+                          //       {isDrawerOpen && <span>Dashboard</span>}
+                          //     </div>
+                          //   </Link>
+                          // </button>
+                          <Link to={"/dashboard"}>
+                            <button
+                              className={
+                                selected
+                                  ? "bg-primary-color text-gray-800 shadow-nm-inset hover: transition-all hover:text-gray-800 px-3 py-2 rounded-md text-sm font-medium border border-zinc-300 block"
+                                  : "bg-primary-color text-gray-500 transition-all hover:text-gray-800 px-3 py-2 rounded-md text-sm font-medium border border-zinc-300 block"
+                              }
+                            >
+                              <div className="flex justify-center items-center gap-x-2 ">
+                                <FaHome></FaHome>
+                                {isDrawerOpen && <span>Dashboard</span>}
+                              </div>
+                            </button>
+                          </Link>
+                        )}
+                      </Tab>
+                      <Tab as={Fragment}>
+                        {({ selected }) => (
+                          <Link to={"addproduct"}>
+                            <button
+                              className={
+                                selected
+                                  ? "bg-primary-color text-gray-800 shadow-nm-inset hover: transition-all hover:text-gray-800 px-3 py-2 rounded-md text-sm font-medium border border-zinc-300 block  "
+                                  : "bg-primary-color text-gray-500  transition-all hover:text-gray-800 px-3 py-2 rounded-md text-sm font-medium border border-zinc-300 block"
+                              }
+                            >
+                              <div className="flex justify-center items-center gap-x-2 min-w-max">
+                                <FaCartPlus></FaCartPlus>
+                                {isDrawerOpen && <span>Add Product</span>}
+                              </div>
+                            </button>
+                          </Link>
+                        )}
+                      </Tab>
+                      <Tab as={Fragment}>
+                        {({ selected }) => (
+                          <Link
+                            to={"myproducts"}
+                            className={({ isActive }) => (isActive ? `` : "")}
+                          >
+                            <button
+                              className={
+                                selected
+                                  ? "bg-primary-color text-gray-800 shadow-nm-inset hover: transition-all hover:text-gray-800 px-3 py-2 rounded-md text-sm font-medium border border-zinc-300 block  "
+                                  : "bg-primary-color text-gray-500  transition-all hover:text-gray-800 px-3 py-2 rounded-md text-sm font-medium border border-zinc-300 block"
+                              }
+                            >
+                              <div className="flex justify-center items-center gap-x-2">
+                                <FaListAlt></FaListAlt>
+                                {isDrawerOpen && <span>My Products</span>}
+                              </div>
+                            </button>
+                          </Link>
+                        )}
+                      </Tab>
+                      <Tab as={Fragment}>
+                        {({ selected }) => (
+                          <Link
+                            to={"mybuyers"}
+                            className={({ isActive }) => (isActive ? `` : "")}
+                          >
+                            <button
+                              className={
+                                selected
+                                  ? "bg-primary-color text-gray-800 shadow-nm-inset hover: transition-all hover:text-gray-800 px-3 py-2 rounded-md text-sm font-medium border border-zinc-300 block  "
+                                  : "bg-primary-color text-gray-500  transition-all hover:text-gray-800 px-3 py-2 rounded-md text-sm font-medium border border-zinc-300 block"
+                              }
+                            >
+                              <div className="flex justify-center items-center gap-x-2">
+                                <FaShopify></FaShopify>
+                                {isDrawerOpen && <span>My Buyers</span>}
+                              </div>
+                            </button>
+                          </Link>
+                        )}
+                      </Tab>
+                    </Tab.List>
+                    <Tab.Panels></Tab.Panels>
+                  </Tab.Group>
+                  {/* <NavLink
                     to={"/dashboard"}
                     className={({ isActive }) =>
                       isActive
-                        ? `bg-primary-color text-gray-800 shadow-nm-inset hover: transition-all hover:text-gray-800 px-3 py-2 rounded-md text-lg font-medium border border-zinc-300 block `
-                        : "bg-primary-color text-gray-500 transition-all hover:text-gray-800 px-3 py-2 rounded-md text-lg font-medium border border-zinc-300 block "
+                        ? `bg-primary-color text-gray-800 shadow-nm-inset hover: transition-all hover:text-gray-800 px-3 py-2 rounded-md text-sm font-medium border border-zinc-300 block `
+                        : "bg-primary-color text-gray-500 transition-all hover:text-gray-800 px-3 py-2 rounded-md text-sm font-medium border border-zinc-300 block "
                     }
                   >
-                    <div className="flex justify-center items-center gap-x-2">
+                    <div className="flex justify-center items-center gap-x-2 ">
                       <FaHome></FaHome>
                       {isDrawerOpen && <span>Dashboard</span>}
                     </div>
-                  </NavLink>
+                  </NavLink> */}
 
-                  <NavLink
+                  {/* <NavLink
                     to={"addproduct"}
                     className={({ isActive }) =>
                       isActive
-                        ? `bg-primary-color text-gray-800 shadow-nm-inset hover: transition-all hover:text-gray-800 px-3 py-2 rounded-md text-lg font-medium border border-zinc-300 block `
-                        : "bg-primary-color text-gray-500  transition-all hover:text-gray-800 px-3 py-2 rounded-md text-lg font-medium border border-zinc-300 block"
+                        ? `bg-primary-color text-gray-800 shadow-nm-inset hover: transition-all hover:text-gray-800 px-3 py-2 rounded-md text-sm font-medium border border-zinc-300 block `
+                        : "bg-primary-color text-gray-500  transition-all hover:text-gray-800 px-3 py-2 rounded-md text-sm font-medium border border-zinc-300 block"
                     }
                   >
-                    <div className="flex justify-center items-center gap-x-2">
+                    <div className="flex justify-center items-center gap-x-2 min-w-max">
                       <FaCartPlus></FaCartPlus>
                       {isDrawerOpen && <span>Add Product</span>}
                     </div>
-                  </NavLink>
-                  <NavLink
+                  </NavLink> */}
+                  {/* <NavLink
                     to={"myproducts"}
                     className={({ isActive }) =>
                       isActive
-                        ? `bg-primary-color text-gray-800 shadow-nm-inset hover: transition-all hover:text-gray-800 px-3 py-2 rounded-md text-lg font-medium border border-zinc-300 block `
-                        : "bg-primary-color text-gray-500  transition-all hover:text-gray-800 px-3 py-2 rounded-md text-lg font-medium border border-zinc-300 block"
+                        ? `bg-primary-color text-gray-800 shadow-nm-inset hover: transition-all hover:text-gray-800 px-3 py-2 rounded-md text-sm font-medium border border-zinc-300 block `
+                        : "bg-primary-color text-gray-500  transition-all hover:text-gray-800 px-3 py-2 rounded-md text-sm font-medium border border-zinc-300 block"
                     }
                   >
                     <div className="flex justify-center items-center gap-x-2">
@@ -95,15 +195,15 @@ const DashboardLayout = () => {
                     to={"mybuyers"}
                     className={({ isActive }) =>
                       isActive
-                        ? `bg-primary-color text-gray-800 shadow-nm-inset hover: transition-all hover:text-gray-800 px-3 py-2 rounded-md text-lg font-medium border border-zinc-300 block `
-                        : "bg-primary-color text-gray-500  transition-all hover:text-gray-800 px-3 py-2 rounded-md text-lg font-medium border border-zinc-300 block"
+                        ? `bg-primary-color text-gray-800 shadow-nm-inset hover: transition-all hover:text-gray-800 px-3 py-2 rounded-md text-sm font-medium border border-zinc-300 block `
+                        : "bg-primary-color text-gray-500  transition-all hover:text-gray-800 px-3 py-2 rounded-md text-sm font-medium border border-zinc-300 block"
                     }
                   >
                     <div className="flex justify-center items-center gap-x-2">
                       <FaShopify></FaShopify>
                       {isDrawerOpen && <span>My Buyers</span>}
                     </div>
-                  </NavLink>
+                  </NavLink> */}
                 </div>
               )}
             </Popover.Panel>
--- src/Pages/Dashboard/SellerDashboard/MyProducts/MyProducts.jsx
+++ src/Pages/Dashboard/SellerDashboard/MyProducts/MyProducts.jsx
@@ -1,9 +1,31 @@
-import React from "react";
+import { Tab } from "@headlessui/react";
+import React, { Fragment } from "react";
 
 const MyProducts = () => {
   return (
     <div>
       <h3 className="text-3xl text-center">MyProducts</h3>
+      <Tab.Group>
+      <Tab.List>
+        <Tab as={Fragment}>
+          {({ selected }) => (
+            /* Use the `selected` state to conditionally style the selected tab. */
+            <button
+              className={
+                selected ? 'bg-blue-500 text-white' : 'bg-white text-black'
+              }
+            >
+              Tab 1
+            </button>
+          )}
+        </Tab>
+        {/* ...  */}
+      </Tab.List>
+      <Tab.Panels>
+        <Tab.Panel>Content 1</Tab.Panel>
+        {/* ... */}
+      </Tab.Panels>
+    </Tab.Group>
     </div>
   );
 };
