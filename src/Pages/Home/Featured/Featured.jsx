61399eb9ae69d8e885acb70fa195b48eee38759f
Rohan1279 <muniruddinrohan@gmail.com>
2023-08-22 23:44:41 +0600

    add featured product api

--- src/Pages/Home/Featured/Featured.jsx
+++ src/Pages/Home/Featured/Featured.jsx
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import Carousel from "../../../components/Carousal/Carousal";
+import { useQuery } from "@tanstack/react-query";
+import { Context } from "../../../contexts/ContextProvider";
 
 function Featured() {
+  const { authInfo } = useContext(Context);
+  const { user } = authInfo;
   const {
     data: featuredProducts = [],
     refetch,
     isLoading,
     isFetching,
   } = useQuery({
-    queryKey: ["products", user?.email],
+    queryKey: ["featured", user?.email],
     queryFn: () =>
       fetch(`${import.meta.env.VITE_SERVER_URL}/featured`).then((res) =>
         res.json()
@@ -16,6 +20,7 @@ function Featured() {
     // refetchOnMount: true,
     // refetchOnWindowFocus: true,
   });
+  console.log(featuredProducts);
   return (
     <div>
       <h1>Featured</h1>
